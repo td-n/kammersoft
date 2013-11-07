0aba91cba54749ae5a2308e65e8b0cc08c11a687
Alvaro Aguilera <alvaro.aguilera@tu-dresden.de>
2013-11-07 20:58:08 +0100

    Entfernung alter Dateien.

--- forbitten.php
+++
@@ -1,8 +0,0 @@
-<?php include ("#mysql.inc.php"); ?>
-<?php include ("#authenticated.inc.php"); ?>
-<?php include ("#log.inc.php")
-$l="forbitten ge&ouml;ffnet - falsche seite";
-logsch ($l);
-echo "<big><big>Sie sind nicht berechtigt diese Seite anzuw&auml;hlen</big></big>"
-?><meta http-equiv="refresh" content="3; URL=start.php" /><?php
-?>
